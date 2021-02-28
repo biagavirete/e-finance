5daeb74a0ccbb0d6302db921321c35b3d20413e0
biagavirete <biagavirete@gmail.com>
2021-02-27 22:55:29 -0300

    feat: create provider for store and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import NavigationBar from './components/AppBar';
+import Routes from './routes';
+import { ThemeProvider } from '@material-ui/core';
+import GlobalStyles from './components/GlobalStyles';
+import { store } from './store';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Provider store={store}>
+        <NavigationBar />
+        <Routes />
+      </Provider>
+    </ThemeProvider>
   );
 }
 
