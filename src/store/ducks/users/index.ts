284356b6908e66a11ef29344ba3bc51a4e3d985a
biagavirete <biagavirete@gmail.com>
2021-02-28 12:24:37 -0300

    feat: add error to user reducer initial state

--- src/store/ducks/users/index.ts
+++ src/store/ducks/users/index.ts
@@ -6,31 +6,36 @@ const INITIAL_STATE: UserState = {
     accessToken: '',
   },
   loading: false,
+  error: false,
+  success: false
 }
 
 const reducer: Reducer<UserState> = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case UserTypes.POST_SIGNUP_REQUEST:
-      return { ...state, loading: true }
+      return { ...state, loading: true, error: false }
     case UserTypes.POST_SIGNUP_SUCCESS:
       return {
         ...state,
         loading: false,
+        success: true,
+        error: false,
         data: action.payload.data
       }
     case UserTypes.POST_SIGNUP_FAILURE:
-      return { ...state, loading: false }
+      return { ...state, loading: false, error: true }
 
     case UserTypes.POST_LOGIN_REQUEST:
       return { ...state, loading: true }
     case UserTypes.POST_LOGIN_SUCCESS:
       return {
         ...state,
+        success: true,
         data: action.payload.data,
         loading: false,
       }
     case UserTypes.POST_LOGIN_FAILURE:
-      return { ...state, loading: false }
+      return { ...state, loading: false, error: true }
     default:
       return state
   }
--- src/store/ducks/users/types.ts
+++ src/store/ducks/users/types.ts
@@ -15,6 +15,8 @@ export interface Users {
 
 export interface UserState {
   readonly loading: boolean;
+  readonly success: boolean;
+  readonly error: boolean;
   data: {
     accessToken: string;
   }
