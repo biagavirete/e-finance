953542a23c8a3361e187f1d653159175ac77e2ea
biagavirete <biagavirete@gmail.com>
2021-02-27 22:54:14 -0300

    feat: create dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../../components/NavBar';
+import Sidebar from '../../components/Sidebar';
+
+function Dashboard() {
+  return (
+    <>
+      <NavBar />
+      <h1>Dashboard</h1>
+    </>
+  )
+}
+
+export default Dashboard;
