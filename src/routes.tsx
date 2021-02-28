55689dcd07e057f9eed783cc999b0b101e101739
biagavirete <biagavirete@gmail.com>
2021-02-28 17:59:14 -0300

    feat: create transactions routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import NewTransaction from './components/NewTransaction';
+import TransactionsTable from './components/TransactionsTable';
 import Currency from './pages/Currency';
 import Dashboard from './pages/Dashboard';
 import Finances from './pages/Finances';
@@ -15,6 +17,9 @@ function Routes() {
       <Route path="/dashboard" exact component={Dashboard} />
       <Route path="/currency" exact component={Currency} />
       <Route path="/finances" exact component={Finances} />
+      <Route path="/newtransaction" exact component={NewTransaction} />
+      <Route path="/transactionslist" exact component={TransactionsTable} />
+
     </Switch>
   )
 }
